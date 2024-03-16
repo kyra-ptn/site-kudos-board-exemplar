f103b70aa6e5b34c790a014c80cff4cbbca9aee3
Trenisha <taglee09@gmail.com>
2024-03-16 00:12:43 -0400

    update

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,23 +1,30 @@
+import express from "express";
+import bodyParser from "body-parser";
+import path from 'path';
+import cors from "cors";
 import boardRoutes from "./routes/boardRoutes";
 import cardRouter from "./routes/cardRoutes";
 
-const express = require("express");
-const bodyParser = require("body-parser");
-const path = require('path');
-const cors = require("cors");
-
 const app = express();
 
-// Serve static files from the React build directory
-app.use(express.json());
+// Middleware
+app.use(bodyParser.json());
 app.use(cors());
 
+// Serve static files from the React build directory
+app.use(express.static(path.join(__dirname, 'path_to_your_react_build_folder')));
 
 // Use card routes under /boards/:boardId
 app.use("/:boardId", cardRouter);
 
+// Use board routes for other routes
 app.use("/", boardRoutes);
 
+// Error handling middleware
+app.use((err: any, req:any, res:any, next:any) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
