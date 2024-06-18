3003d8e791ea649b3c5892ea04f360be1b51a2ae
Kyra Patton <144150344+kyra-ptn@users.noreply.github.com>
2024-06-18 08:04:54 -0700

    Update NewBoardForm.js

--- frontend/src/components/newboardform/NewBoardForm.js
+++ frontend/src/components/newboardform/NewBoardForm.js
@@ -15,7 +15,7 @@ const NewBoardForm = ({ onSuccess, onClose }) => {
         alert("Please fill out all fields");
         return; 
       }
-      await axios.post("https://site-kudos-board-exemplar.onrender.com/boards", {
+      await axios.post("https://site-kudos-board-backend-exemplar.onrender.com/boards", {
         title: newBoardTitle,
         category: newBoardCategory,
         owner: newBoardAuthor,
